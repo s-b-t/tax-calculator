c7b8bbead5f32049c6dc5a7645a23534aeedd657
Steve-Tobias <sblaket@gmail.com>
2024-06-09 21:56:43 -0600

    modified: main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 # Boldens text using ANSI escape characters for user to clearly discern what the program is trying to validate in order to enhance user experience
 def boldText(text):
@@ -36,6 +37,17 @@ def getFloatInput(prompt):
                 print(boldText('Invalid input. Please enter a valid number.'))
                 print()
 
+def getDate(prompt):
+    while True:
+        dateInput = input(prompt).strip()
+        try:
+            valid_date = datetime.strptime(dateInput, "%m/%d/%y")
+            return valid_date.strftime("%m/%d/%y")
+        except ValueError:
+            print()
+            print(boldText('Invalid date format. Please enter the date in MM/DD/YY format.'))
+            print()
+
 def saveToTextFile(outputData):
     while True:
         fileName = input("Enter the file name you'd like to save your earnings/tax breakdown to. " + boldText("You do not need to add a file extension.") + " A .txt file will be saved by default: ")
@@ -63,9 +75,11 @@ print()
 input('Grab your most recent paystub and press ' + boldText('[Enter]') + ' to continue... ')
 print()
 name = getName('Enter your name: ').upper()
+startDate = getDate('Enter the start of the pay period ' + boldText('(MM/DD/YY)') + ': ')
+endDate = getDate('Enter the end date of the pay period ' + boldText('(MM/DD/YY)') + ': ')
 regHours = getFloatInput('Enter your regular hours worked in the pay period (weekly, bi-weekly, etc.): 🕘 ')
 hourlyRate = getFloatInput('Enter your hourly rate: 💲 ')
-cashTips = getFloatInput('Enter your cash tips/commissions earned (Type 0 if N/A): 💲 ')
+cashTips = getFloatInput('Enter your cash tips/commissions earned (type 0 if N/A): 💲 ')
 taxWithheld = getFloatInput('Enter your taxes withheld: 💲 ')
 deductionsWithheld = getFloatInput('Enter your total amounts deducted (Benefits, 401K, etc.): 💲 ')
 print('\n')
@@ -83,8 +97,8 @@ checkAmount = (netPay - cashTips)
 
 # Breakdown for user
 outputData = (
-    '----------------------------------------------------\n'
-    + boldText(f'EARNINGS/TAX BREAKDOWN FOR {name} __ THRU __:') + '\n'
+    '-------------------------------------------------------\n'
+    + f'EARNINGS/TAX BREAKDOWN FOR {name} {startDate} THRU {endDate}:' + '\n'
     + f'Your Tax Rate: {round(taxRate, 3) * 100} %\n'
 )
 
@@ -108,7 +122,7 @@ if hourlyPay != grossEarnings:
 else: 
     outputData += f'Hourly Pay Total/Gross Earnings: 💲 {round(grossEarnings, 2)}\n'
 
-outputData += '----------------------------------------------------'
+outputData += '-------------------------------------------------------'
 
 # Print the output data
 print(outputData)
@@ -131,4 +145,3 @@ while True:
         print(boldText("Please answer Yes or No."))
         print()
         continue
-
